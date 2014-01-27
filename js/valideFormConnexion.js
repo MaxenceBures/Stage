74c5bc218b0cd54a5011ed04d5fcdf1ca5118f66
MaxenceBures <maxence.bures@gmail.com>
2014-01-27 12:29:22 +0100

    TEs

--- js/valideFormConnexion.js
+++ js/valideFormConnexion.js
@@ -1,14 +1,3 @@
-$(document).ready(function()
-{
-	$("#frmConnexion").validate({
-		rules:{
-			login_cnx :"required",
-			mdp_cnx	  :"required"
-		},
-		
-		messages:{
-			login_cnx :{required:"Veuillez saisir votre login"},
-			mdp_cnx   :{required:"Veuillez saisir votre mot de passe"},
-		},
-	});
+$(document).ready(function(){
+    $("#frmConnexion").validate();
 });
