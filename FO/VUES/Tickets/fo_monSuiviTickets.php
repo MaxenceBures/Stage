0e01d06b92dc3fb1c73ff4631e75077bd99d06c1
MaxenceBures <maxence.bures@gmail.com>
2014-02-05 14:17:09 +0100

    Sql

--- FO/VUES/Tickets/fo_monSuiviTickets.php
+++ FO/VUES/Tickets/fo_monSuiviTickets.php
@@ -27,12 +27,12 @@
     		<div class="col span_12">
        			<table class="default-table">
 					<tr>
-						<th>Num</th>
-						<th>Date</th>
-						<th>Salle</th>
-						<th>Matériel</th>
-						<th>Problème</th>
-						<th>Constat</th>
+						<th>Code</th>
+						<th>INC</th>
+						<th>Demandeur</th>
+						<th>Technicien</th>
+						<th>INT</th>
+						<th>DESC</th>
 					</tr>
 <?php			
 							//	$lesTickets  = getAllDemandes($sLogin ,1) ;
@@ -40,12 +40,12 @@
 								{
 ?>
 						<tr>
-							<td><?php echo $unTicket->Tic_Num ;  ?></td>
-							<td><?php echo modifierDate($unTicket ->Tic_DatCre); ?></td>
-							<td><?php echo $unTicket->Tic_Salle ; ?></td>
-							<td><?php echo $unTicket->Tic_Materiel ; ?></td>
-							<td><?php echo $unTicket->Cat_Libelle ;?></td>	
-							<td><?php echo stripslashes($unTicket->Tic_Constat) ; ?></td>	
+							<td><?php echo $unTicket->INT_CODE ;  ?></td>
+							<td><?php echo $unTicket->INC_LIBELLE; ?></td>
+							<td><?php echo $unTicket->INC_DEMANDE ; ?></td>
+							<td><?php echo $unTicket->INT_TECHNICIEN ; ?></td>
+							<td><?php echo $unTicket->INT_LIBELLE ;?></td>	
+							<td><?php echo $unTicket->INT_DESCRIPTION ; ?></td>	
 						</tr>
 
 
