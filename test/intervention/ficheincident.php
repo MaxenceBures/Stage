112be736db74afa2d7be08ad100322e05c87e8fe
MaxenceBures <maxence.bures@gmail.com>
2014-02-20 09:41:50 +0100

    tableau

--- test/intervention/ficheincident.php
+++ test/intervention/ficheincident.php
@@ -3,7 +3,7 @@ require_once('function.php');
 $con = connecter();
 $test = $_POST['test'];
 $enreg = mysql_fetch_assoc(mysql_query("SELECT INC_CODE, INC_LIBELLE, INC_DATEDEMANDE, UTI_LOGIN, INC_DATECLOTURE, ENT_RAISONSOCIALE FROM INCIDENT, UTILISATEUR, ENTREPRISE WHERE INC_CODE ='".$test."'AND INCIDENT.UTI_CODE = UTILISATEUR.UTI_CODE AND INCIDENT.ENT_CODE = ENTREPRISE.ENT_CODE"));
-$enreg2 = mysqli_query($con,"SELECT INT_CODE, INT_LIBELLE, INT_DESCRIPTION, INT_HEUREDEB, INT_HEUREFIN, INT_DATEINTER, UTI_LOGIN   FROM INTERVENTION, UTILISATEUR WHERE INC_CODE ='".$test."' AND INT_TECHNICIEN = UTILISATEUR.UTI_CODE") or die(mysqli_error($con));
+$enreg2 = mysql_query("SELECT INT_CODE, INT_LIBELLE, INT_DESCRIPTION, INT_HEUREDEB, INT_HEUREFIN, INT_DATEINTER, UTI_LOGIN   FROM INTERVENTION, UTILISATEUR WHERE INC_CODE ='".$test."' AND INT_TECHNICIEN = UTILISATEUR.UTI_CODE") ;
 
 //INC_CODE, URG_CODE, UTI_CODE, ENT_CODE, ETA_CODE, INC_LIBELLE, INC_DESCRIPTION, INC_DATEDEMANDE, INC_DECOMPTE, INC_VALIDATION, INC_DEMANDE, INC_TYPE, INC_DATECLOTURE 
 ?>
@@ -41,17 +41,90 @@ $enreg2 = mysqli_query($con,"SELECT INT_CODE, INT_LIBELLE, INT_DESCRIPTION, INT_
 					</td>
 			</tr>
 </table>
-<table border='1'>
+
 
 <?php
+$row = 0;
+//$result = mysql_query("SELECT id,link FROM mytable Order By id DESC LIMIT 0,5");
+$new_array[] = $row;
+// while ($row = mysql_fetch_array($enreg2)) {
+//     $new_array[$row['INT_CODE']] = $row;
+//     $new_array[$row['INT_DATEINTER']] = $row;
+// }
+//mysql_close($db);// close mysql then do other job with set_time_limit(59)
 
-while($enreg2 = mysqli_fetch_array($enreg2) or die("Error: ".mysqli_error($con)))
+while ($row = mysql_fetch_array($enreg2)) 
+{
+    $new_array[$row['INT_CODE']]['INT_CODE'] = $row['INT_CODE'];
+    $new_array[$row['INT_CODE']]['INT_DATEINTER'] = $row['INT_DATEINTER'];
+    $new_array[$row['INT_CODE']]['INT_HEUREDEB'] = $row['INT_HEUREDEB'];
+    $new_array[$row['INT_CODE']]['INT_HEUREFIN'] = $row['INT_HEUREFIN'];
+    $new_array[$row['INT_CODE']]['UTI_LOGIN'] = $row['UTI_LOGIN'];
+    $new_array[$row['INT_CODE']]['INT_LIBELLE'] = $row['INT_LIBELLE'];
+    $new_array[$row['INT_CODE']]['INT_DESCRIPTION'] = $row['INT_DESCRIPTION'];
+}
+foreach($new_array as $array){
+   ?> <table border='1'><tr>
+					<td>
+						<label for="num">Intervention n° : </label>
+					</td>
+					<td>
+						<input type="text" id="num" required="" value="<?php echo $array['INT_CODE']; ?>" name="num" readonly/>
+					</td>
+				
+					<td>
+						<label for="date">effectuée le : </label>
+					</td>
+					<td>
+						<input type="text" id="date" required="" value="<?php echo $array['INT_DATEINTER']; ?>" name="date" readonly/>
+					</td>
+				
+					<td>
+						<label for="deb">De : </label>
+					</td>
+					<td>
+						<input type="text" id="deb" required="" value="<?php echo $array['INT_HEUREDEB']; ?>" name="deb"readonly/>
+					</td>
+				
+					<td>
+						<label for="fin">A : </label>
+					</td>
+					<td>
+						<input type="text" id="fin" name="fin" value="<?php echo $array['INT_HEUREFIN']; ?>"readonly/>
+					</td>
+			
+					<td>
+						<label for="inter">Par : </label>
+					</td>
+					<td>
+						<input type="text" id="inter" name="inter" value="<?php echo $array['UTI_LOGIN']; ?> "readonly/>
+					</td>
+				</tr>
+				<tr>
+					<td>
+						<label for="libelle">Libelle : </label>
+					</td>
+					<td>
+						<input type="text" id="libelle" name="libelle" value="<?php echo $array['INT_LIBELLE']; ?>" readonly/>
+					</td>
+				</tr>
+				<tr>
+					<td>
+						<label for="description">Description : </label>
+					</td>
+					<td>
+						<textarea value=""><?php echo $array['INT_DESCRIPTION']; ?></textarea>
+					</td>
+				</tr>
+				</table><?php
+}
+/*
+while($enreg2 = mysql_fetch_array($enreg2)){
 //	    $rows[] = mysqli_fetch_array($enreg2);
 //foreach($rows as $enreg2)
-  {
+  var_dump($enreg2);
 ?>
 			<hr>
-			
 				<tr>
 					<td>
 						<label for="num">Intervention n° : </label>
@@ -104,5 +177,6 @@ while($enreg2 = mysqli_fetch_array($enreg2) or die("Error: ".mysqli_error($con))
 						<textarea value=""><?php echo($enreg2['INT_DESCRIPTION']); ?></textarea>
 					</td>
 				</tr>
+				 
 				 <?php }?>
-				 </table>
+				 </table>*/
