f40ec36bda621213acf219649b67d5dae41c846e
MaxenceBures <maxence.bures@gmail.com>
2014-02-20 10:17:33 +0100

    fonctionnel

--- test/intervention/ficheincident.php
+++ test/intervention/ficheincident.php
@@ -2,10 +2,8 @@
 require_once('function.php');
 $con = connecter();
 $test = $_POST['test'];
-$enreg = mysql_fetch_assoc(mysql_query("SELECT INC_CODE, INC_LIBELLE, INC_DATEDEMANDE, UTI_LOGIN, INC_DATECLOTURE, ENT_RAISONSOCIALE FROM INCIDENT, UTILISATEUR, ENTREPRISE WHERE INC_CODE ='".$test."'AND INCIDENT.UTI_CODE = UTILISATEUR.UTI_CODE AND INCIDENT.ENT_CODE = ENTREPRISE.ENT_CODE"));
+$enreg = mysqli_fetch_assoc(mysqli_query($con, "SELECT INC_CODE, INC_LIBELLE, INC_DATEDEMANDE, UTI_LOGIN, INC_DATECLOTURE, ENT_RAISONSOCIALE FROM INCIDENT, UTILISATEUR, ENTREPRISE WHERE INC_CODE ='".$test."'AND INCIDENT.UTI_CODE = UTILISATEUR.UTI_CODE AND INCIDENT.ENT_CODE = ENTREPRISE.ENT_CODE"));
 $enreg2 = mysqli_query($con, "SELECT INT_CODE, INT_LIBELLE, INT_DESCRIPTION, INT_HEUREDEB, INT_HEUREFIN, INT_DATEINTER, UTI_LOGIN   FROM INTERVENTION, UTILISATEUR WHERE INC_CODE ='".$test."' AND INT_TECHNICIEN = UTILISATEUR.UTI_CODE") ;
-
-//INC_CODE, URG_CODE, UTI_CODE, ENT_CODE, ETA_CODE, INC_LIBELLE, INC_DESCRIPTION, INC_DATEDEMANDE, INC_DECOMPTE, INC_VALIDATION, INC_DEMANDE, INC_TYPE, INC_DATECLOTURE 
 ?>
 <table class="style1">
 				<tr>
@@ -63,10 +61,13 @@ while ($row = mysqli_fetch_array($enreg2))
     $new_array[$row['INT_CODE']]['INT_LIBELLE'] = $row['INT_LIBELLE'];
     $new_array[$row['INT_CODE']]['INT_DESCRIPTION'] = $row['INT_DESCRIPTION'];
 }
+//if(is_array($row)){
 foreach($new_array as $array){
-   ?>
+  if( $array['INT_CODE'] == "") {echo "test";} 
+  else {  ?>
    <hr>
-   		<table border='1'><tr>
+   		<table border='1'>
+   		<tr>
 					<td>
 						<label for="num">Intervention n° : </label>
 					</td>
@@ -119,8 +120,8 @@ foreach($new_array as $array){
 					</td>
 				</tr>
 				</table>
-				<?php
-}
+				<?php }
+}//}
 /*
 while($enreg2 = mysql_fetch_array($enreg2)){
 //	    $rows[] = mysqli_fetch_array($enreg2);
