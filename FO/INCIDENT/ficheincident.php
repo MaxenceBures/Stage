25c093f4af0b0576f0514f6a5bdb96e0f97bf887
MaxenceBures <maxence.bures@gmail.com>
2014-02-24 11:44:22 +0100

    t

--- FO/INCIDENT/ficheincident.php
+++ FO/INCIDENT/ficheincident.php
@@ -2,9 +2,8 @@
 // require_once('function.php');
 $con = connecter();
 $test = $_POST['test'];
-$enreg = mysqli_fetch_array(mysqli_query($con, "SELECT INC_CODE, INC_LIBELLE, INC_DATEDEMANDE, UTI_LOGIN, INC_DATECLOTURE, ENT_RAISONSOCIALE, INC_CLOTURE FROM INCIDENT, UTILISATEUR, ENTREPRISE WHERE INC_CODE = '".$test."' AND INCIDENT.UTI_CODE = UTILISATEUR.UTI_CODE AND INCIDENT.ENT_CODE = ENTREPRISE.ENT_CODE"));
+$enreg = mysqli_fetch_array(mysqli_query($con, "SELECT INC_CODE, INC_LIBELLE, INC_DATEDEMANDE, UTI_LOGIN, INC_DATECLOTURE, ENT_RAISONSOCIALE, INC_CLOTURE, ETA_CODE FROM INCIDENT, UTILISATEUR, ENTREPRISE WHERE INC_CODE = '".$test."' AND INCIDENT.UTI_CODE = UTILISATEUR.UTI_CODE AND INCIDENT.ENT_CODE = ENTREPRISE.ENT_CODE"));
 $enreg2 = mysqli_query($con, "SELECT INT_CODE, INT_LIBELLE, INT_DESCRIPTION, INT_HEUREDEB, INT_HEUREFIN, INT_DATEINTER, UTI_LOGIN   FROM INTERVENTION, UTILISATEUR WHERE INC_CODE ='".$test."' AND INT_TECHNICIEN = UTILISATEUR.UTI_CODE") ;
-//ob_start();
 ?>
 <table class="style1">
 				<tr>
@@ -123,85 +122,23 @@ foreach($new_array as $array){
 				</table>
 				
 				<?php }
+if ($enreg['ETA_CODE'] == '5') 
+{
+	?>
+		<form action="INC/pdf.php" method="POST">
+		<input type="submit" value="PDF"></input>
+		<input type="hidden" name="test" id="test" value="<?php echo ($enreg['INC_CODE'])?>">
+		</form>
+	<?php
+}
+elseif ($enreg['ETA_CODE'] == '3' OR $enreg['ETA_CODE'] == '4') 
+{
+	?>	
+		<form action="?page=cloturer" method="POST">
+		<input type="submit" value="Cloturer"></input>
+		<input type="hidden" name="test" id="test" value="<?php echo ($enreg['INC_CODE'])?>">
+		</form>
+	<?php	
+}	
 
-}?><form action="INC/pdf.php" method="POST">
-<input type="submit" value="PDF"></input>
-<input type="hidden" name="test" id="test" value="<?php echo ($enreg['INC_CODE'])?>">
-</form>
-<form action="?page=cloturer" method="POST">
-<input type="submit" value="Cloturer"></input>
-<input type="hidden" name="test" id="test" value="<?php echo ($enreg['INC_CODE'])?>">
-</form>
-<!-- <?php/* //}
-/*
-while($enreg2 = mysql_fetch_array($enreg2)){
-//	    $rows[] = mysqli_fetch_array($enreg2);
-//foreach($rows as $enreg2)
-  var_dump($enreg2);
-
-			<hr>
-				<tr>
-					<td>
-						<label for="num">Intervention n° : </label>
-					</td>
-					<td>
-						<input type="text" id="num" required="" value="<?php echo($enreg2['INT_CODE']); ?>" name="num" readonly/>
-					</td>
-				
-					<td>
-						<label for="date">effectuée le : </label>
-					</td>
-					<td>
-						<input type="text" id="date" required="" value="<?php echo($enreg2['INT_DATEINTER']); ?>" name="date" readonly/>
-					</td>
-				
-					<td>
-						<label for="deb">De : </label>
-					</td>
-					<td>
-						<input type="text" id="deb" required="" value="<?php echo($enreg2['INT_HEUREDEB']); ?>" name="deb"readonly/>
-					</td>
-				
-					<td>
-						<label for="fin">A : </label>
-					</td>
-					<td>
-						<input type="text" id="fin" name="fin" value="<?php echo($enreg2['INT_HEUREFIN']); ?>"readonly/>
-					</td>
-			
-					<td>
-						<label for="inter">Par : </label>
-					</td>
-					<td>
-						<input type="text" id="inter" name="inter" value="<?php echo($enreg2['UTI_LOGIN']); ?> "readonly/>
-					</td>
-				</tr>
-				<tr>
-					<td>
-						<label for="libelle">Libelle : </label>
-					</td>
-					<td>
-						<input type="text" id="libelle" name="libelle" value="<?php echo($enreg2['INT_LIBELLE']); ?>" readonly/>
-					</td>
-				</tr>
-				<tr>
-					<td>
-						<label for="description">Description : </label>
-					</td>
-					<td>
-						<textarea value=""><?php echo($enreg2['INT_DESCRIPTION']); ?></textarea>
-					</td>
-				</tr>
-				 
-				 <?php }?>
-// 				 </table>*$content = ob_get_clean();
-// require('html2pdf/html2pdf.class.php');
-// try{
-// 	$pdf = new HTML2PDF('P','A4','fr');
-// 	$pdf-> writeHTML($content);
-// 	$pdf->Output('test.pdf');
-// }
-// catch(HTML2PDF_exception $e){
-// 	die($e);
-// }*/
-?>
+}?>
